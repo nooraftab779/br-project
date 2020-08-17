a60c110be2ced7e681744041e653104a11891738
nooraftab779 <58308273+nooraftab779@users.noreply.github.com>
2020-08-17 15:12:22 +0500

    heavy panga

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import TopBar from "../src/components/TopBar";
+import "react-toastify/dist/ReactToastify.css";
+import FlavorPage from "./components/FlavorPage";
+import Products from "./components/product";
+import SignInPage from "./components/Login/SignInPage";
+import SignUpPage from "./components/Login/SignUpPage";
+import EditPage from "./components/EditPage";
+import AddPage from "./components/AddPage";
+import IceCreamPage from "./components/IceCreamPage";
+import CartPage from "./components/CartPage";
+import MainPage from "./components/MainPage";
+import Footer from "./components/footer";
+import AboutUs from "./components/AboutUs";
 
 function App() {
+  const [cart, setcart] = React.useState(localStorage.getItem("items"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="maincon" style={{ backgroundColor: "#d8c4b798" }}>
+        <div style={{ backgroundColor: "rgb(252, 230, 233)" }}>
+          <TopBar cart={cart} setcart={setcart} />
+          <Switch>
+            <Route
+              path="/CartPage"
+              exact
+              component={() => <CartPage setcart={setcart} />}
+            />
+            <Route path="/AboutUs" exact component={AboutUs} />
+            <Route path="/SignInPage" exact component={SignInPage} />
+            <Route path="/SignUpPage" exact component={SignUpPage} />
+            <Route
+              path="/products"
+              exact
+              component={() => <FlavorPage setcart={setcart} />}
+            />
+            <Route
+              path="/IceCreamPage/:id"
+              exact
+              component={(props) => <IceCreamPage setcart={setcart} />}
+            />
+            <Route path="/products/EditPage/:id" exact component={EditPage} />
+            <Route path="/products/AddPage" exact component={AddPage} />
+            <Route path="/" exact component={MainPage} />
+            <Redirect to="/not-found" />
+          </Switch>
+          <Footer />
+        </div>
+      </div>
+    </Router>
   );
 }
 
